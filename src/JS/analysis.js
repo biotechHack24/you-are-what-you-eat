f332bafb46aaa26f21665ece0649c6498df0b6c0
Florian Braun <braunf25@proton.me>
2024-11-08 10:37:20 +0100

    basic js and css file stucture

---
+++ src/JS/analysis.js
@@ -0,0 +1,12 @@
+import styles from "../CSS/analyse.css";
+
+export default function Home() {
+    return (
+        <main className={styles.main}>
+            <h1>Analysis</h1>
+            <p>
+                This is the analysis page.
+            </p>
+        </main>
+    );
+}
---
+++ src/JS/statistics.js
@@ -0,0 +1,12 @@
+import styles from "../CSS/stats.css";
+
+export default function Home() {
+    return (
+        <main className={styles.main}>
+            <h1>Statistics</h1>
+            <p>
+                This is the statistics page.
+            </p>
+        </main>
+    );
+}
