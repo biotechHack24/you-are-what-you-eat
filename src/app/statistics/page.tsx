c8c7edc671b3dba92246bc6f5bab01205f24be03
Rumen <rumenmitov@protonmail.com>
2024-11-09 09:42:32 +0100

    feat: statistics outline

--- src/app/statistics/page.tsx
+++ src/app/statistics/page.tsx
@@ -15,17 +15,23 @@ export default function Statistics() {
     };
 
     return (
-        <main className={styles.main}>
-            <h1>Statistics</h1>
-            <h2>Nutrients</h2>
-            <p>Carbohydrates: {example_obj.nutrients.carbohydrates}</p>
-            <p>Protein: {example_obj.nutrients.protein}</p>
-            <p>Minerals: {example_obj.nutrients.minerals}</p>
-            <p>Dairy: {example_obj.nutrients.dairy}</p>
-            <hr />
-            <p>Overall Health Sore: {example_obj.health_score}</p>
-            <h2>Environment</h2>
-            <p>Score: {example_obj.environmental_score}</p>
-        </main>
+        <section className={styles.main}>
+            <h1 className={styles.heading}>Statistics</h1>
+            <section className={styles.data}>
+                <h2 className={styles.section_heading}>Nutrients</h2>
+                <section className={styles.data_subsection}>
+                    <p>Carbohydrates: {example_obj.nutrients.carbohydrates}</p>
+                    <p>Protein: {example_obj.nutrients.protein}</p>
+                    <p>Minerals: {example_obj.nutrients.minerals}</p>
+                    <p>Dairy: {example_obj.nutrients.dairy}</p>
+                    <hr />
+                    <p>Overall Health Sore: {example_obj.health_score}</p>
+                </section>
+                <h2 className={styles.section_heading}>Environment</h2>
+                <section className={styles.data_subsection}>
+                    <p>Score: {example_obj.environmental_score}</p>
+                </section>
+            </section>
+        </section>
     );
 }
