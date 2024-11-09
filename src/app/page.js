39143ddf362ee636798656a193a00351e37b346e
Florian Braun <braunf25@proton.me>
2024-11-09 18:12:33 +0100

    cleaning up

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,10 @@
-import styles from "./page.module.css";
+import styles from "./globals.css";
 import Analysis from "./analysis/page";
 import Statistics from "./statistics/page";
 
 export default function Home() {
   return (
       <div>
-          <p>home page</p>
           <Analysis />
       </div>
   );
